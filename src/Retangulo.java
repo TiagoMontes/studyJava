9f8d58e59b887aff3c96594a5a3c73c6b87226c1
TiagoMontes <tiagosantos6303@gmail.com>
2024-01-17 16:13:46 -0500

    initial commit

---
+++ src/Retangulo.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Retangulo {
+
+    public static void main(String[] args) {
+        float altura, largura, area, perimetro;
+        Scanner entrada;
+        entrada = new Scanner(System.in);
+        System.out.print("Digite o valor da altura do retângulo: ");
+        altura = entrada.nextFloat();
+
+        System.out.print("Digite o valor da largura do retângulo: ");
+        largura = entrada.nextFloat();
+
+        area = calcularArea(altura, largura);
+        perimetro = calcularPerimetro(altura, largura);
+
+        mostrarMensagem("O valor da área é ", area);
+        mostrarMensagem("O valor do perímetro é ", perimetro);
+    }
+
+    public static float calcularArea(float a, float b) {
+        return a*b;
+    }
+
+    public static float calcularPerimetro(float a, float b) {
+        return 2*a + 2*b;
+    }
+
+    public static void mostrarMensagem(String msg, float vlr) {
+        System.out.println(msg+vlr);
+    }
+}
